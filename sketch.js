9d98250f73b3c387dc4eccecc86b0aa35bbf7376
procodingclass <88050205+procodingclass@users.noreply.github.com>
2021-08-27 12:55:16 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,8 @@
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
-}
+  createCanvas(400,400);
+ }
 
 function draw() {
-  background(255,255,255);  
-  drawSprites();
+  background(220);  
+  
 }
